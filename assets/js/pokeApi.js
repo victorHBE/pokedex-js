e13059ec2beb95f09f4d704167b04b46df1b8c37
Victor <victorbenevides2002@gmail.com>
2023-09-13 20:37:58 -0400

    incrementando novos dados na model vindo da api

--- assets/js/pokeApi.js
+++ assets/js/pokeApi.js
@@ -11,6 +11,19 @@ function convertPokeApiToPokemonDetail(pokemonDetail) {
     pokemon.type = type;
     pokemon.types = types;
     pokemon.photo = pokemonDetail.sprites.other.dream_world.front_default;
+    pokemon.baseExp = pokemonDetail.base_experience;
+    const stats = pokemonDetail.stats.map((stat) => stat.base_stat);
+    const [hp, attack, defense, specialAttack, specialDefense, speed] = stats;
+    pokemon.stats.hp = hp;
+    pokemon.stats.attack = attack;
+    pokemon.stats.defense = defense;
+    pokemon.stats.specialAttack = specialAttack;
+    pokemon.stats.specialDefense = specialDefense;
+    pokemon.stats.speed = speed;
+    const abilities = pokemonDetail.abilities.map((abilitySlot) => abilitySlot.ability.name);
+    pokemon.abilities = abilities;
+    pokemon.height = pokemonDetail.height;
+    pokemon.weight = pokemonDetail.weight;
 
     return pokemon
 }
--- assets/js/pokemonModel.js
+++ assets/js/pokemonModel.js
@@ -5,4 +5,9 @@ class Pokemon {
     type;
     types = [];
     photo;
+    abilities = [];
+    baseExp;
+    stats = [];
+    height;//Altura
+    weight;//Peso
 }
